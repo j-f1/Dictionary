db543cc7e9a23beb4392c2c1a72c1b58d368a0d1
Jed Fox <git@jedfox.com>
2021-04-13 21:35:41 -0400

    Initial Commit

---
+++ Dictionary/DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryApp.swift
+//  Dictionary
+//
+//  Created by Jed Fox on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
