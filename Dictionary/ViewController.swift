b514c2a595ee1aa6f1223911d21f72b1c86aac3a
Jed Fox <git@jedfox.com>
2021-04-15 12:56:11 -0400

    definitions!

--- Dictionary/ViewController.swift
+++ Dictionary/ViewController.swift
@@ -6,18 +6,42 @@
 //
 
 import UIKit
+import ZIPFoundation
+import WebKit
 
 class ViewController: UIViewController {
+
+  static var archive = ZIPFoundation.Archive(url: Bundle.main.url(forResource: "dict", withExtension: "zip")!, accessMode: .read)!
 
   var word: String? {
     didSet {
       self.navigationItem.title = word
+      if self.webView != nil {
+        loadPage()
+      }
     }
   }
 
+  @IBOutlet weak var webView: WKWebView!
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    loadPage()
+  }
+
+  func loadPage() {
+    var data = """
+      <meta name="viewport" content="width=device-width" />
+      <link rel="stylesheet" href="styles.css" />
+      <style>body { margin: 20px; } </style>
+    """.data(using: .utf8)!
+    if let word = word,
+       let entry = Self.archive["defs/\(word).html"] {
+      _ = try! Self.archive.extract(entry) { chunk in
+        data += chunk
+      }
+      self.webView.load(data, mimeType: "text/html", characterEncodingName: "utf-8", baseURL: Bundle.main.resourceURL!)
+    }
   }
 
   override func viewDidDisappear(_ animated: Bool) {
