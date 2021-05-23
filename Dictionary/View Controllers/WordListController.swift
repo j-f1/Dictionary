4cd113846c402f04e7a4e89fab20c9d2471d6a87
Jed Fox <git@jedfox.com>
2021-05-23 17:11:38 -0400

    Refactor out the business logic of WordsTableViewController

---
+++ Dictionary/View Controllers/WordListController.swift
@@ -0,0 +1,77 @@
+//
+//  WordListController.swift
+//  Dictionary
+//
+//  Created by Jed Fox on 5/23/21.
+//
+
+import UIKit
+
+fileprivate enum CellIdentifier {
+  static let word = "WordRow"
+}
+
+class WordListController: NSObject, UITableViewDataSource, UISearchResultsUpdating {
+
+  init(tableView: UITableView) {
+    self.tableView = tableView
+    super.init()
+
+    searchController.searchResultsUpdater = self
+    tableView.dataSource = self
+  }
+
+  let searchController: UISearchController = {
+    let sc = UISearchController(searchResultsController: nil)
+    sc.hidesNavigationBarDuringPresentation = false
+    sc.automaticallyShowsCancelButton = false
+    sc.obscuresBackgroundDuringPresentation = false
+    sc.searchBar.searchBarStyle = .prominent
+    sc.searchBar.autocorrectionType = .default
+    sc.searchBar.autocapitalizationType = .none
+    sc.searchBar.returnKeyType = .done
+    sc.searchBar.enablesReturnKeyAutomatically = false
+    return sc
+  }()
+
+  let tableView: UITableView!
+
+  var allWords: [WordLetter]? {
+    didSet {
+      tableView.reloadData()
+    }
+  }
+
+  // MARK: - UISearchResultsUpdating
+  func updateSearchResults(for searchController: UISearchController) {
+    guard let query = searchController.searchBar.text?.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) else { return }
+    guard let section = allWords!.firstIndex(where: { query.first == $0.letter.first }) else { return }
+    if let row = allWords![section].words.firstIndex(where: { $0 > query }) {
+      tableView.scrollToRow(at: IndexPath(row: row == 0 ? row : row - 1, section: section), at: .top, animated: false)
+    }
+  }
+
+  // MARK: - Table view data source
+
+  func numberOfSections(in tableView: UITableView) -> Int {
+    allWords?.count ?? 0
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    allWords![section].words.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.word, for: indexPath)
+    cell.textLabel?.text = allWords![indexPath.section].words[indexPath.row]
+    return cell
+  }
+
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    nil
+  }
+
+  func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    allWords?.map { $0.letter.uppercased() }
+  }
+}
--- Dictionary/View Controllers/WordsTableViewController.swift
+++ Dictionary/View Controllers/WordsTableViewController.swift
@@ -9,24 +9,8 @@ import UIKit
 import Combine
 import Defaults
 
-fileprivate enum CellIdentifier {
-  static let word = "WordRow"
-}
+class WordsTableViewController: UIViewController, UITableViewDelegate {
 
-class WordsTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISplitViewControllerDelegate {
-
-  let searchController: UISearchController = {
-    let sc = UISearchController(searchResultsController: nil)
-    sc.hidesNavigationBarDuringPresentation = false
-    sc.automaticallyShowsCancelButton = false
-    sc.obscuresBackgroundDuringPresentation = false
-    sc.searchBar.searchBarStyle = .prominent
-    sc.searchBar.autocorrectionType = .default
-    sc.searchBar.autocapitalizationType = .none
-    sc.searchBar.returnKeyType = .done
-    sc.searchBar.enablesReturnKeyAutomatically = false
-    return sc
-  }()
 
   lazy var detailVC = {
     self.splitViewController?.viewController(for: .secondary) ?? self.storyboard?.instantiateViewController(identifier: VCIdentifier.detail)
@@ -38,22 +22,20 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
 
   let history = BackForwardStack<IndexPath>()
   var subscriptions: Set<AnyCancellable> = Set()
-
-  var allWords: [WordLetter]?
+  var controller: WordListController!
 
   // MARK: - View Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    searchController.searchResultsUpdater = self
+    self.controller = WordListController(tableView: tableView)
+    tableView.delegate = self
+
     navigationItem.hidesSearchBarWhenScrolling = false
-    navigationItem.titleView = searchController.searchBar
+    navigationItem.titleView = controller.searchController.searchBar
 
     pasteButton.transform = .init(translationX: 0, y: 26)
-
-    tableView.dataSource = self
-    tableView.delegate = self
 
     self.toolbarItems = [
       UIBarButtonItem(
@@ -96,7 +78,7 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
         if let detailVC = self.detailVC as? UINavigationController,
            let customVC = detailVC.topViewController as? DetailViewController,
            let indexPath = self.history.state {
-          customVC.word = self.allWords![indexPath.section].words[indexPath.row]
+          customVC.word = self.controller.allWords![indexPath.section].words[indexPath.row]
           if self.tableView.indexPathForSelectedRow != indexPath {
             self.tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
             self.tableView.scrollToRow(at: indexPath, at: .none, animated: false)
@@ -116,14 +98,13 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
   }
 
   override func didMove(toParent parent: UIViewController?) {
-    self.splitViewController?.delegate = self
-    if self.allWords == nil {
-      self.allWords = DictionaryProvider.loadWords(from: "boot")
+    if controller.allWords == nil {
+      controller.allWords = DictionaryProvider.loadWords(from: "boot")
       tableView.reloadData()
       DispatchQueue.global(qos: .userInitiated).async {
         let allWords = DictionaryProvider.loadWords(from: "words-by-letter")
         DispatchQueue.main.async {
-          self.allWords = allWords
+          self.controller.allWords = allWords
           self.tableView.reloadData()
           self.updatePasteButton()
         }
@@ -165,7 +146,7 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
            !detected.contains(.number),
            !detected.contains(.probableWebURL) {
           if let copiedString = UIPasteboard.general.string,
-             let (word, indexPath) = find(query: copiedString, in: allWords!) {
+             let (word, indexPath) = find(query: copiedString, in: controller.allWords!) {
             if pasteTarget == nil || word != pasteLabel.text {
               pasteLabel.text = word
               pasteTarget = indexPath
@@ -188,39 +169,6 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
     }
   }
 
-  // MARK: - UISearchResultsUpdating
-  func updateSearchResults(for searchController: UISearchController) {
-    guard let query = searchController.searchBar.text?.lowercased().trimmingCharacters(in: .whitespacesAndNewlines) else { return }
-    guard let section = allWords!.firstIndex(where: { query.first == $0.letter.first }) else { return }
-    if let row = allWords![section].words.firstIndex(where: { $0 > query }) {
-      tableView.scrollToRow(at: IndexPath(row: row == 0 ? row : row - 1, section: section), at: .top, animated: false)
-    }
-  }
-
-  // MARK: - Table view data source
-
-  func numberOfSections(in tableView: UITableView) -> Int {
-    allWords?.count ?? 0
-  }
-
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    allWords![section].words.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier.word, for: indexPath)
-    cell.textLabel?.text = allWords![indexPath.section].words[indexPath.row]
-    return cell
-  }
-
-  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    nil
-  }
-
-  func sectionIndexTitles(for tableView: UITableView) -> [String]? {
-    allWords?.map { $0.letter.uppercased() }
-  }
-
   // MARK: - Table view delegate
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     history.move(to: indexPath)
@@ -228,7 +176,7 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
 
   // MARK: - Navigation Actions
   func goToRandomWord(_ sender: Any) {
-    let lengths = allWords!.map(\.words.count)
+    let lengths = controller.allWords!.map(\.words.count)
     let totalLength = lengths.reduce(0, +)
     var row = Int.random(in: 0..<totalLength)
     var section = 0
@@ -245,12 +193,12 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
   var canGoToNext: Bool {
     self.history.state != nil &&
       self.history.state
-      != IndexPath(row: allWords!.last!.words.count - 1, section: allWords!.count - 1)
+      != IndexPath(row: controller.allWords!.last!.words.count - 1, section: controller.allWords!.count - 1)
   }
   func goToNext() {
     assert(canGoToNext)
     let currentPath = self.history.state!
-    if currentPath.row == allWords![currentPath.section].words.count - 1 {
+    if currentPath.row == controller.allWords![currentPath.section].words.count - 1 {
       self.history.move(to: IndexPath(row: 0, section: currentPath.section + 1))
     } else {
       self.history.move(to: IndexPath(row: currentPath.row + 1, section: currentPath.section))
@@ -264,7 +212,7 @@ class WordsTableViewController: UIViewController, UITableViewDataSource, UITable
     assert(canGoToPrevious)
     let currentPath = self.history.state!
     if currentPath.row == 0 {
-      self.history.move(to: IndexPath(row: allWords![currentPath.section - 1].words.count - 1, section: currentPath.section - 1))
+      self.history.move(to: IndexPath(row: controller.allWords![currentPath.section - 1].words.count - 1, section: currentPath.section - 1))
     } else {
       self.history.move(to: IndexPath(row: currentPath.row - 1, section: currentPath.section))
     }
